54495145b27cc205c13c92a8074981c5618c8f6c
Viinicius-Muller <zandreviniciusmuller@gmail.com>
2025-07-26 14:40:10 -0300

    first commit

---
+++ src/main/java/com/github/viinicius_muller/inventory_stock_manager/InventoryStockManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.viinicius_muller.inventory_stock_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryStockManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryStockManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/viinicius_muller/inventory_stock_manager/InventoryStockManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viinicius_muller.inventory_stock_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryStockManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
