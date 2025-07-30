0a4a41e906ef034dcb58d88ce2a2bcfa30b79306
Viinicius-Muller <zandreviniciusmuller@gmail.com>
2025-07-30 20:30:47 -0300

    feat: add CategoriaController, Spring-starter-web dependency

---
+++ src/main/java/com/github/viinicius_muller/inventory_stock_manager/controller/CategoriaController.java
@@ -0,0 +1,17 @@
+package com.github.viinicius_muller.inventory_stock_manager.controller;
+
+import jakarta.transaction.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("categorias")
+public class CategoriaController {
+
+    @PostMapping
+    public void adicionarCategoria(@RequestBody String json) {
+        System.out.println(json);
+    }
+}
