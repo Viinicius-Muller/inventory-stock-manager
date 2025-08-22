4322a72460d46f44d0ef2d513fad940be8728e91
Viinicius-Muller <zandreviniciusmuller@gmail.com>
2025-08-21 21:24:58 -0300

    refactor: modified Migration V8

--- src/main/resources/db/migration/V8__create-table-movimentacoes.sql
+++ src/main/resources/db/migration/V8__create-table-movimentacoes.sql
@@ -1,8 +1,8 @@
 CREATE TABLE movimentacoes(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    produto_id INT,
-    quantidade INT,
-    data DATE,
-    tipo VARCHAR(30),
+    produto_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    data DATE NOT NULL,
+    tipo VARCHAR(30) NOT NULL,
     FOREIGN KEY (produto_id) REFERENCES produtos(id)
 )
