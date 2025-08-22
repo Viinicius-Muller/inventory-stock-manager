2e7b6b0dad2bce692f4f53de6d1d756c1464860f
Viinicius-Muller <zandreviniciusmuller@gmail.com>
2025-08-21 21:35:28 -0300

    refactor: modified ProductListData

--- src/main/java/com/github/viinicius_muller/inventory_stock_manager/produto/ProductListData.java
+++ src/main/java/com/github/viinicius_muller/inventory_stock_manager/produto/ProductListData.java
@@ -3,13 +3,16 @@ package com.github.viinicius_muller.inventory_stock_manager.produto;
 import com.github.viinicius_muller.inventory_stock_manager.categoria.Categoria;
 import jakarta.persistence.Column;
 
-public record ProductListData(String produto, String descricao_do_produto, String categoria, int estoque_atual, int estoque_minimo) {
+import java.math.BigDecimal;
+
+public record ProductListData(Long id, String produto, String descricao_do_produto, String categoria, int estoque_atual, BigDecimal preco_atual) {
     public ProductListData(Produto produto) {
         this(
+                produto.getId(),
                 produto.getNome(),
+                produto.getCategoria().getCategoria(), //get category's name
                 produto.getDescricao(),
-                produto.getCategoria().getCategoria(), //gets only the category's name
                 produto.getEstoque_atual(),
-                produto.getEstoque_minimo());
+                produto.getPreco_atual());
     }
 }
