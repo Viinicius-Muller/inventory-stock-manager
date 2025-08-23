125a01bcd79515ccb75fe3359813c21140aa9da0
Viinicius-Muller <zandreviniciusmuller@gmail.com>
2025-08-22 21:00:21 -0300

    feat: add EntityNotFoundException Handler, add DataIntegrityViolationException Handler

--- src/main/java/com/github/viinicius_muller/inventory_stock_manager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/viinicius_muller/inventory_stock_manager/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.github.viinicius_muller.inventory_stock_manager.exception;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,7 +17,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<Object> handleTypeMismatch(MethodArgumentTypeMismatchException err) {
-        String msg = "O parâmetro \""+err.getName()+"\" recebeu um valor inválido: "+err.getValue()+".";
+        String msg = "O parâmetro '"+err.getName()+"' recebeu um valor inválido: "+err.getValue()+".";
 
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
@@ -24,5 +26,41 @@ public class GlobalExceptionHandler {
         body.put("message: ", msg);
 
         return new ResponseEntity<>(body,HttpStatus.BAD_REQUEST);
+    }
+
+    //Objetos não encotrados pelo ‘id’
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleNoEntity(EntityNotFoundException err) {
+        String msg = "Objeto não encontrado na base de dados.";
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", "Id Inválido");
+        body.put("message: ", msg);
+
+        return  new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    //Objeto com mesmo valor único em certa coluna
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleSameValues(DataIntegrityViolationException err) {
+        Throwable rootCause = err.getMostSpecificCause();
+        String causeMsg = rootCause.getMessage();
+
+        String[] partes = causeMsg.split("'");
+
+        String value = partes[1];
+        String field = partes[3];
+
+        String msg = "O valor '"+value+"' já existe no campo '"+field+"'.";
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", "Conflito de Dados");
+        body.put("message: ", msg);
+
+        return  new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
